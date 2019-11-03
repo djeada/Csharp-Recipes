9eff2a24c3655f372cd75bbb7fff43c77c6cb08d
Adam Djellouli <01102712@pw.edu.pl>
2019-11-03 23:35:08 +0100

    A simple game using WPF

--- Duck/MainWindow.xaml.cs
+++ Duck/MainWindow.xaml.cs
@@ -20,9 +20,44 @@ namespace Duck
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random r = new Random();
+        List<Image> kaczki = new List<Image>();
+
         public MainWindow()
         {
             InitializeComponent();
+            System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += dispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Interval = TimeSpan.FromMilliseconds(50);
+            dispatcherTimer.Start();
+            kaczki.Add(kaczka1);
+            kaczki.Add(kaczka2);
+            kaczki.Add(kaczka3);
+        }
+
+        Point GetMousePos()
+        {
+            Point p = Mouse.GetPosition(Application.Current.MainWindow);
+            p.X -= 50;
+            p.Y -= 50;
+            return p;
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            Thickness m = target.Margin;
+            m.Left = GetMousePos().X;
+            m.Top = GetMousePos().Y;
+            target.Margin = m;
+
+            foreach (Image k in kaczki)
+            {
+                m = k.Margin;
+                m.Left -= 10;
+                m.Top -= r.Next(-5, 5);
+                k.Margin = m;
+            }
         }
     }
 }
