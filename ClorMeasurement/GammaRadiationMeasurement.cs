f77de83f5fbcd476895c480c46568c2ff53ddb35
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-11-25 14:06:57 +0100

    większość gotowa

---
+++ ClorMeasurement/GammaRadiationMeasurement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClorMeasurement
+{
+    public class GammaRadiationMeasurement : Measurement
+    {
+        public double radiationLevel { get; set; } = 0;
+        public GammaRadiationMeasurement(string name = "No name!") : base(name)
+        {
+
+        }
+        public override void Print()
+        {
+            base.Print();
+        }
+    }
+}
