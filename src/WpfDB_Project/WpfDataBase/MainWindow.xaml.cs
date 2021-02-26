0020bb002304b4c9e82469b6f887965deed45709
djeada <addjellouli1@gmail.com>
2021-02-26 13:23:43 +0100

    added wpf db project

---
+++ src/WpfDB_Project/WpfDataBase/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDataBase
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlConnection conn = new SqlConnection();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            string connString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Adam\source\repos\WpfDataBase\WpfDataBase\DatabaseEmployees.mdf;Integrated Security=True;";
+            conn.ConnectionString = connString;
+            conn.Open();
+            updateTable();
+        }
+
+        void updateTable()
+        {
+            SqlCommand command = new SqlCommand("SELECT * FROM Persons", conn);
+            command.ExecuteNonQuery();
+            SqlDataAdapter dataAdp = new SqlDataAdapter(command);
+            DataTable dt = new DataTable("Persons");
+            dataAdp.Fill(dt);
+            tableGrid.ItemsSource = dt.DefaultView;
+        }
+
+        private void InsertButton_Click(object sender, RoutedEventArgs e)
+        {
+            string name = "'" + nameText.Text + "'";
+            string surname = "'" + surnameText.Text + "'";
+            string phone = "'" + phoneText.Text + "'";
+            string query = String.Format("INSERT INTO Persons(Name, Surname, Number) VALUES ({0},{1},{2})", name, surname, phone);
+            SqlCommand command = new SqlCommand(query, conn);
+            command.ExecuteNonQuery();
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            string name = "'" + nameText.Text + "'";
+            string surname = "'" + surnameText.Text + "'";
+            string phone = "'" + phoneText.Text + "'";
+            string query = String.Format("DELETE FROM Persons Where Name={0} AND Surname={1} AND Number={2}", name, surname, phone);
+            SqlCommand command = new SqlCommand(query, conn);
+            command.ExecuteNonQuery();
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            updateTable();
+        }
+    }
+}
---
+++ src/WpfDB_Project/WpfDataBase/WindowLogin.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfDataBase
+{
+    /// <summary>
+    /// Interaction logic for WindowLogin.xaml
+    /// </summary>
+    public partial class WindowLogin : Window
+    {
+        public WindowLogin()
+        {
+            InitializeComponent();
+        }
+
+        public string ConnectionString { get; private set; }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            string connString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Adam\source\repos\WpfDataBase\WpfDataBase\DatabaseEmployees.mdf;Integrated Security=True;";
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = connString;
+
+            try
+            {
+                if(conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+                String query = "SELECT COUNT(1) FROM Logins WHERE Username=@Username AND Password=@Password";
+                SqlCommand sqlCmd = new SqlCommand(query, conn);
+                sqlCmd.CommandType = CommandType.Text;
+                sqlCmd.Parameters.AddWithValue("@Username", txtUserName.Text);
+                sqlCmd.Parameters.AddWithValue("@Password", txtPassword.Password);
+                int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                if (count == 1)
+                {
+                    MainWindow board = new MainWindow();
+                    board.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Wrong credentials");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
